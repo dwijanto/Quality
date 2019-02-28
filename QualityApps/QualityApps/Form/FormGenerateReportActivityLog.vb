147d1a07e57a4fc6c828f171d5cf622f18b6f881
dwijanto <dwijanto@yahoo.com>
2019-02-28 15:36:44 +0800

    Adding PivotTable in ActivityLog Report
    Adding PivotTable in ActivityLog Report

--- QualityApps/QualityApps/Form/FormGenerateReportActivityLog.vb
+++ QualityApps/QualityApps/Form/FormGenerateReportActivityLog.vb
@@ -1,4 +1,6 @@
-﻿Public Class FormGenerateReportActivityLog
+﻿Imports Microsoft.Office.Interop
+
+Public Class FormGenerateReportActivityLog
 
     Public Shared myForm As FormGenerateReportActivityLog
     Private SaveFileName As String = String.Empty
@@ -20,7 +22,7 @@
         SaveFileDialog1.FileName = String.Format("ActivityLog-{0:yyyyMMdd}.xlsx", Date.Today)
         Dim criteria = String.Format(" where u.userid in (select quality.getsubordinate('{0}')) and activitydate >= '{1:yyyy-MM-dd}' and activitydate <= '{2:yyyy-MM-dd}'", myIdentity.userid.ToLower, DateTimePicker1.Value.Date, DateTimePicker2.Value.Date)
         If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
-            Dim myReport As ExportToExcelFile = New ExportToExcelFile(Me, myController.GetSQLSTRReport(criteria), IO.Path.GetDirectoryName(SaveFileDialog1.FileName), IO.Path.GetFileName(SaveFileDialog1.FileName), AddressOf FormatReport, AddressOf PivotCallback, 1, "\templates\excelTemplate.xltx")
+            Dim myReport As ExportToExcelFile = New ExportToExcelFile(Me, myController.GetSQLSTRReport(criteria), IO.Path.GetDirectoryName(SaveFileDialog1.FileName), IO.Path.GetFileName(SaveFileDialog1.FileName), AddressOf FormatReport, AddressOf PivotCallback, 2, "\templates\activitylog.xltx")
             myReport.Run(Me, New EventArgs)
         End If
     End Sub
@@ -29,8 +31,26 @@
         'Throw New NotImplementedException
     End Sub
 
-    Private Sub PivotCallback()
-        'Throw New NotImplementedException
+    Private Sub PivotCallback(ByRef sender As Object, ByRef e As EventArgs)
+        Dim oXl As Excel.Application = Nothing
+        Dim owb As Excel.Workbook = CType(sender, Excel.Workbook)
+        oXl = owb.Parent
+        owb.Worksheets(2).select()
+        Dim osheet = owb.Worksheets(2)
+        Dim orange = osheet.Range("A2")
+        If osheet.cells(2, 2).text.ToString = "" Then
+            Err.Raise(100, Description:="Data not available.")
+        End If
+        osheet.name = "RawData"
+        owb.Names.Add("db", RefersToR1C1:="=OFFSET('RawData'!R1C1,0,0,COUNTA('RawData'!C1),COUNTA('RawData'!R1))")
+
+        owb.Worksheets(1).select()
+        osheet = owb.Worksheets(1)
+        osheet.PivotTables("PivotTable1").ChangePivotCache(owb.PivotCaches.Create(Excel.XlPivotTableSourceType.xlDatabase, SourceData:="db"))
+        'oXl.Run("ShowFG")
+        osheet.PivotTables("PivotTable1").PivotCache.Refresh()
+        osheet.pivottables("PivotTable1").SaveData = True
+        owb.RefreshAll()
     End Sub
 
 End Class
